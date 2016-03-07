34d7d51d311c7c92e3862d24d910d1c0ab654f53
Huy Nguyen <qhuydtvt@gmail.com>
2016-03-07 22:20:11 +0700

    Changed JSON format

--- Webservice/iliat_gmat/questions.py
+++ Webservice/iliat_gmat/questions.py
@@ -13,7 +13,15 @@ class Question(Document):
     right_answer = IntField()
     explanation = StringField()
 
+    # def get_json(self, json_encoder):
+    #     return json_encoder.encode(self.to_mongo())
+
 def from_questions_to_json_string(question_list):
     json_encoder = JSONEncoder()
-    json_list = [str(json_encoder.encode(question.to_mongo())) for question in question_list]
-    return ",<br>".join(json_list)
+    json_list = [question.to_json(sort_keys=True, indent=4, separators=(',', ': ')) for question in question_list]
+    print(json_list)
+    return (",<br>".join(json_list)).replace("\n", "<br>").replace(" ", "&nbsp")
+
+# def from_json_to_string(json_object):
+#     print(json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': ')))
+#     return json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': '))
